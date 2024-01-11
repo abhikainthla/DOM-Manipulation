153417d0a58e2bedf7757a8d1e55e3d8c259c4d4
abhikainthla <105478999+abhikainthla@users.noreply.github.com>
2024-01-11 13:53:36 +0530

    Created a project with the help of dom mainuplation

---
+++ script.js
@@ -0,0 +1,18 @@
+let main = document.getElementById("main");
+
+main.style.backgroundColor = "yellow";
+
+main.style.margin = "20px";
+
+main.style.padding = "10px";
+
+main.style.fontSize ="18px";
+
+main.style.fontWeight ="bold";
+
+main.style.height ="200px";
+
+main.style.width ="300px";
+
+
+main.style.color ="blue";
